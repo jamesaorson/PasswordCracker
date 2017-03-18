cad210991f5d26e783aedb97145e287d4b4176ba
JamesOsborne <james.osborne112@gmail.com>
2017-03-18 19:13:52 -0400

    Now reads from bible file and fills dictionary. Slowness is from StringBuilder append in hash().

--- PasswordCracker/PasswordCracker.cs
+++ PasswordCracker/PasswordCracker.cs
@@ -7,14 +7,25 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace PasswordCracker {
-    class PasswordCracker {
+    static class PasswordCracker {
+        private static MD5 md5 = MD5.Create();
         static void Main(string[] args) {
-            string input = Console.ReadLine();
-            Console.WriteLine(hash(input));
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            
+            string[] words = File.ReadAllText("bible.txt").Split((string[])null,
+                             StringSplitOptions.RemoveEmptyEntries);
+            
+            for (int i = 0; i < words.Length; ++i) {
+                string temp = hash(words[i]);
+                if (!dict.ContainsKey(temp)) {
+                    dict.Add(temp, words[i]);
+                }
+            }
+
+            Console.WriteLine("done with " + dict.Count() + " words");
         }
 
         private static string hash(string input) {
-            MD5 md5 = MD5.Create();
             byte[] inputBytes = Encoding.ASCII.GetBytes(input);
             byte[] hashBytes = md5.ComputeHash(inputBytes);
 
@@ -23,7 +34,6 @@ namespace PasswordCracker {
             for (int i = 0; i < hashBytes.Length; ++i) {
                 hash.Append(hashBytes[i].ToString("x2"));
             }
-            
             return hash.ToString();
         }
     }
